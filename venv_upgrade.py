a5888446eede5bd778cf9a30f70f112ed06961eb
Mathias Rav <m@git.strova.dk>
2025-01-08 08:31:03 +0100

    Update for 3.11+ using importlib.metadata

--- venv_upgrade.py
+++ venv_upgrade.py
@@ -1,4 +1,4 @@
-# Copyright 2018, Mathias Rav <m@git.strova.dk>
+# Copyright 2025, Mathias Rav <m@git.strova.dk>
 # SPDX-License-Identifier: GPL-2+
 
 """
@@ -9,39 +9,40 @@ with the current version of Python.
 import argparse
 import glob
 import os
-import pkg_resources
+import importlib.metadata
 import re
 import subprocess
 import sys
-import venv
 
 
-parser = argparse.ArgumentParser(
-    description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter()
-)
+parser = argparse.ArgumentParser(description=__doc__.strip())
 parser.add_argument("directory")
 
 
 def main():
     args = parser.parse_args()
-    sys_path_entry, = glob.glob(
+    (sys_path_entry,) = glob.glob(
         os.path.join(args.directory, "lib/python*/site-packages")
     )
-    mo, = re.finditer("python(\d)\.(\d)", sys_path_entry)
+    (mo,) = re.finditer("python(\d)\.(\d)", sys_path_entry)
     major, minor = map(int, mo.group(1, 2))
     if (major, minor) == sys.version_info[:2]:
         parser.error(
             "Target venv has same Python version as current: %s.%s" % (major, minor)
         )
-    entries = list(pkg_resources.find_on_path(None, sys_path_entry))
-    print(entries)
+    entries = sorted(
+        [
+            f"{d.name}=={d.version}"
+            for d in importlib.metadata.distributions(path=[sys_path_entry])
+        ]
+    )
+    print(" ".join(entries))
     if not entries:
         parser.error("venv contains no installed packages")
     os.rename(args.directory, args.directory + "~")
-    venv.main([args.directory])
+    subprocess.check_call([sys.executable, "-m", "venv", args.directory])
     subprocess.check_call(
-        [os.path.join(args.directory, "bin/pip"), "install"]
-        + ["%s==%s" % (o.project_name, o.version) for o in entries]
+        [os.path.join(args.directory, "bin/pip"), "install"] + entries
     )
 
 
